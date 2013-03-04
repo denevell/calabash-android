f7c8bf21308a7a334ec4627f835b1bc28fa9fc4e
root <root@debian>
2013-03-04 10:53:03 +0000

    Added wait to rotation step

--- ruby-gem/lib/calabash-android/steps/rotation_steps.rb
+++ ruby-gem/lib/calabash-android/steps/rotation_steps.rb
@@ -1,7 +1,9 @@
 Then /^I rotate the device to landscape$/ do
   performAction('set_activity_orientation', 'landscape')
+  performAction('wait', 4)
 end
 
 Then /^I rotate the device to portrait$/ do
   performAction('set_activity_orientation', 'portrait')
+  performAction('wait', 4)
 end
